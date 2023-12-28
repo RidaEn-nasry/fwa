2e6ea3d8f0341d416c5f6da0d49abc88d21a1588
Rida Ennasry <rida.ennasry@gmail.com>
2023-12-28 21:33:14 +0100

    add signIn

--- ex00/Cinema-folder/src/main/java/fr.42.cinema/servlets/UsersServlet.java
+++ ex00/Cinema-folder/src/main/java/fr.42.cinema/servlets/UsersServlet.java
@@ -10,8 +10,13 @@ import org.springframework.context.ApplicationContext;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebServlet;
-
 import fr.fortytwo.cinema.services.UsersService;
+
+// ---------
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/users")
 public class UsersServlet extends HttpServlet {
@@ -23,6 +28,19 @@ public class UsersServlet extends HttpServlet {
         ApplicationContext springContext = (ApplicationContext) context.getAttribute("springContext");
         usersService = springContext.getBean(UsersService.class);
         assert usersService != null;
+    }
+
+    @Override
+    // reponding with signIn.html page (form)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/html/signIn.html").forward(request, response);
+
+    }
+
+    @Override
+    public void destroy() {
+        usersService = null;
     }
 
 }
---
+++ ex00/Cinema-folder/target/Cinema-1.0-SNAPSHOT/WEB-INF/classes/sql/schema.sql
@@ -0,0 +1,14 @@
+
+
+-- postgreslq
+CREATE TABLE IF NOT EXISTS public.cinema (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+);  
+
+
+
+
